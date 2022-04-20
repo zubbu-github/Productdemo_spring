d2f6a7c51ed01f3a5c12c50f967a9e71ec5cb7cc
LENOVO <mdzubeir2468@gmail.com>
2022-04-20 17:50:01 +0530

    Product demo using spring

---
+++ Product/src/main/java/com/c2tc/batch/product/ProductController.java
@@ -0,0 +1,71 @@
+package com.c2tc.batch.product;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+	
+	 // RESTful API methods for Retrieval operations 
+	@GetMapping("/product")
+	public List<Product> list() 
+	{
+		return service.listAll();
+	}
+	@GetMapping("/product/{id}")
+	public ResponseEntity<Product> get(@PathVariable Integer id) 
+	{
+	 try {
+		 Product product = service.get(id);
+		 return new ResponseEntity<Product>(product, HttpStatus.OK);
+	 }
+		catch(NoSuchElementException e) 
+	    {
+	     return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	 // RESTful API method for Create operation 
+	@PostMapping("/product")
+	public void add(@RequestBody Product product) {
+		service.save(product);
+	}
+	
+	// RESTful API method for Update operation 
+	@PutMapping("/product/{id}")
+	public ResponseEntity<?> update(@RequestBody Product product,@PathVariable Integer id){
+		try {
+			Product existProduct = service.get(id);
+			service.save(product);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		catch (NoSuchElementException e) 
+		{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	 // RESTful API method for Delete operation 
+	@DeleteMapping("/product/{id}")
+	public void delete(@PathVariable Integer id)
+	{
+		service.delete(id);
+	}
+
+
+	
+}
---
+++ Product/src/test/java/com/c2tc/batch/product/ProductModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c2tc.batch.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
